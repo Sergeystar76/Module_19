0846a327ff1d96643bb00f1abe767490cb9fc063
Sergeystar76 <kertex@yandex.ru>
2024-11-11 20:02:04 +0300

    add file

--- Django/task1/models.py
+++ Django/task1/models.py
@@ -11,8 +11,9 @@ class Buyer(models.Model):# модель представляющая покуп
 
 class Game(models.Model):  #модель представляющая игру.
     title = models.CharField(max_length=100) #название игры
-    cost = models.DecimalField(max_digits=150, decimal_places= 4) # цена
-    size = models.DecimalField(max_digits=150, decimal_places= 4) # размер файлов игры
-    description = models.TextField() # описание(неограниченное кол-во текста)
+    cost = models.DecimalField(max_digits=150, decimal_places=4)# цена
+    size = models.DecimalField(max_digits=150, decimal_places=4)# размер файлов игры
+    description = models.TextField()# описание(неограниченное кол-во текста)
     age_limited = models.BooleanField(default=False)
-    buyer = models.ManyToManyField(Buyer, related_name='game')# - покупатель обладающий игрой. У каждого покупателя может быть игра и у каждой игры может быть несколько обладателей.
+    buyer = models.ManyToManyField(Buyer, related_name='game')# - покупатель обладающий игрой.
+    # У каждого покупателя может быть игра и у каждой игры может быть несколько обладателей.
